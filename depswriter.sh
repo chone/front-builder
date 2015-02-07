f106865026de00935cf40d7f7976a9e3ba0477d3
Nathan XIAO <chonebz@gmail.com>
2015-02-07 22:09:14 +0800

    Update scan path for support all project.

--- depswriter.sh
+++ depswriter.sh
@@ -3,8 +3,7 @@
 bin=`dirname $0`
 
 $bin/../closure-library/closure/bin/build/depswriter.py \
-  --root_with_prefix="$bin/../closure-templates ../../../closure-templates" \
-  --root_with_prefix="$bin/../mem ../../../mem" \
+  --root_with_prefix="$bin/../ ../../../" \
   > $bin/../deps.js
 
 
